bc3ecb6841202e9d57d5fdd23157b2634aec3ddb
Terrence Chan <terrencechan0830@gmail.com>
2023-04-12 02:07:17 +0800

    23:00

---
+++ src/components/banner/Banner.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <section id='home' className="w-full h-[800px] pb-20 flex border-b-[1px] border-b-black">
+        <div>
+            <h4 classsName="text-lg font-normal">WELCOME TO MY WORLD!</h4>
+        </div>
+    </section>
+  )
+}
+
+export default Banner
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,19 +1,29 @@
 import React from 'react';
+import { Link } from 'react-scroll';
 import {logo} from "../../assets/index";
 import { NavLinksdata } from "../../constants";
 
 const Navbar = () => {
   return (
-    <div className="w-full h-20 mx-auto flex justify-between items-center">
+    <div className="w-full h-20 mx-auto flex justify-between items-center font-titleFont">
       <div>
         <img src={logo} alt="logo" />
       </div>
       <div>
         <ul className="flex items-center gap-10">
           {
-            NavLinksdata.map((navlink) => (
-              <li key={navlink._id}>
-                {navlink.title}
+            NavLinksdata.map(({_id, title, link}) => (
+              <li className="text-base font-normal text-gray-400 tracking-wide cursor-pointer hover:text-designColor duration-300" key={_id}>
+                <Link
+                  activeClass="active"
+                  to={link}
+                  spy={true}
+                  smooth={true}
+                  offset={-70}
+                  duration={500}
+                >
+                  {title}
+                </Link>
               </li>
             ))
           }
