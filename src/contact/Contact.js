982a0d43bb87cc3eb03974dca72c44ef6a09132d
Terrence Chan <terrence.tf.chan@pccw.com>
2023-05-04 18:04:10 +0800

    55:00

--- src/contact/Contact.js
+++ src/contact/Contact.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Title from '../components/layouts/Title'
-import { contactImg } from '../assets'
+import LeftContact from './LeftContact'
+
 
 const Contact = () => {
   return (
@@ -10,11 +11,32 @@ const Contact = () => {
       </div>
       <div className='w-full'>
         <div className='w-full h-auto flex justify-between'>
-            <div className='w-[35%] h-full bg-gradient-to-r from-[#1e2024] to-[#23272b] p-8 rounded-lg shadow-shadowOne'>
-                <img className="" src={contactImg} alt="contactImg" />
-            </div>
-            <div className='w-[60%] h-full flex flex-col justify-between '>
-
+            <LeftContact />
+            <div className='w-[60%] h-full py-10 bg-gradient-to-r from-[#1e2024] to-[#23272b] flex flex-col gap-8 p-8 rounded-lg shadow-ShadowOne'>
+              <form className='w-full flex flex-col gap-6 py-2'>
+                <div className='w-full flex gap-10'>
+                  <div className='w-1/2 flex flex-col gap-4'>
+                    <p className='text-sm text-gray-400 uppercase tracking-wide'>Your Name</p>
+                    <input className='contactInput' type='text' />
+                  </div>
+                  <div className='w-1/2 flex flex-col gap-4'>
+                    <p className='text-sm text-gray-400 uppercase tracking-wide'>Phone Number</p>
+                    <input className='contactInput' type='text' />
+                  </div>
+                </div>
+                <div className='flex flex-col gap-4'>
+                  <p className='text-sm text-gray-400 uppercase tracking-wide'>Email</p>
+                  <input className='contactInput' type='email' />
+                </div>
+                <div className='flex flex-col gap-4'>
+                  <p className='text-sm text-gray-400 uppercase tracking-wide'>Subject</p>
+                  <input className='contactInput' type='text' />
+                </div>
+                <div className='flex flex-col gap-4'>
+                  <p className='text-sm text-gray-400 uppercase tracking-wide'>Your Message</p>
+                  <textarea cols='30' rows="10" type='text' />
+                </div>
+              </form>
             </div>
         </div>
       </div>
