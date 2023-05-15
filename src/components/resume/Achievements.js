1645e488a90cd178586098ee1a81a7e204f6d9b7
terrencechan0830 <42678918+terrencechan0830@users.noreply.github.com>
2023-05-16 01:11:20 +0800

    upload cert logo

--- src/components/resume/Achievements.js
+++ src/components/resume/Achievements.js
@@ -6,7 +6,7 @@ const Experience = () => {
   return (
     <motion.div initial={{opacity:0}} animate={{opacity:1, transition:{duration:.5}}}>
         <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
-            <p className="text-sm text-designColor tracking-[4px]">Certificates</p>
+            {/* <p className="text-sm text-designColor tracking-[4px]">Certificates</p> */}
             <h2 className="text-3xl md:text-4xl font-bold">Achievements</h2>
         </div>
         <div className='mt-14 w-full h-[900px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10'>
--- src/components/resume/Skills.js
+++ src/components/resume/Skills.js
@@ -7,7 +7,7 @@ const Skills = () => {
     <motion.div initial={{opacity:0}} animate={{opacity:1, transition:{duration:.5}}} className='w-full flex gap-20'>
         <div className='w-1/2'>
             <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
-                <p className="text-sm text-designColor tracking-[4px]">Features</p>
+                {/* <p className="text-sm text-designColor tracking-[4px]">Features</p> */}
                 <h2 className="text-3xl md:text-4xl font-bold">Programming Languages</h2>
             </div>
             <div className="mt-14 w-full flex flex-col gap-8">
@@ -22,7 +22,7 @@ const Skills = () => {
 
         <div className='w-1/2'>
             <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
-                <p className="text-sm text-designColor tracking-[4px]">Features</p>
+                {/* <p className="text-sm text-designColor tracking-[4px]">Features</p> */}
                 <h2 className="text-3xl md:text-4xl font-bold">Software & Tools</h2>
             </div>
             <div className="mt-14 w-full flex flex-col gap-8">
