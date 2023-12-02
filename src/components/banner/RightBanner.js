137eaf0b425b379e3151b715bce3e81236b724d2
Terrence Chan <terrencechan0830@gmail.com>
2023-12-02 16:54:30 +0800

    update breakpoint

--- src/components/banner/RightBanner.js
+++ src/components/banner/RightBanner.js
@@ -4,7 +4,7 @@ import { bannerImg } from '../../assets'
 const RightBanner = () => {
   return (
     <div className="w-full lgl:w-1/2 flex justify-center items-center relative">
-        <img className="w-[300px] h-[400px] lgl:w-[550px] lgl:h-[625px] z-10" src={bannerImg} alt="bannerImg" />
+        <img className="w-[364px] h-[405px] lgl:w-[500px] lgl:h-[550px] xl:w-[550px] xl:h-[600px] z-10" src={bannerImg} alt="bannerImg" />
         <div className="absolute bottom-0 w-[350px] h-[300px] lgl:w-[500px] lgl:h-[500px] bg-gradient-to-b from-[#1e2024] to-[#202327] shadow-ShadowOne flex justify-center items-center">
         </div>
     </div>
